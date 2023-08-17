eda4d9ee7796235b6134ad4011260bc381e2b523
Stephen Fu <stephenfu1@gmail.com>
2023-08-17 11:26:40 -0400

    task 2 in progress

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,27 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, dispatch } = useContext(AppContext);
+
+    const updateBudget = (val)=>{
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: val,
+        })
+    }
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: £
+                <input
+                    required='required'
+                    type='number'
+                    id='budget'
+                    value={budget}                    
+                    style={{ size: 10 }}
+                    onChange={(event) => updateBudget(event.target.value)}>
+                </input>
+            </span>
         </div>
     );
 };
